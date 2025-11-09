ac75243a4bb676d4d500a894993aaf67d7cfc8f1
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-11-09 18:39:25 +0530

    Time: 53 ms (5.15%) | Memory: 47.3 MB (5.21%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int atMost(String s, int k){
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        while(j < n){
+            map.put(s.charAt(j), map.getOrDefault(s.charAt(j),0)+1);
+
+            while(map.size() > k && i <= j){
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)-1);
+                if(map.get(s.charAt(i)) == 0){
+                    map.remove(s.charAt(i));
+                }
+                i++;
+            }
+
+            ans += j-i+1;
+            j++;
+        }
+        return ans;
+    }
+    public int numberOfSubstrings(String s) {
+        // HEre taking 'K' as 3, because it is given that we have 3 chars.
+        int k = 3;
+        return atMost(s, k) - atMost(s, k-1);
+    }
+}
