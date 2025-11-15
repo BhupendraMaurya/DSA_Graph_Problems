732e7fa2e29ec55bde6958927c5c7dff30930474
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-11-15 14:47:12 +0530

    Time: 20 ms (43.19%) | Memory: 47.8 MB (7.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,50 +1,48 @@
 class Solution {
     
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> mp = new HashMap<>();
-
-        // Build frequency map of characters in t
-        for (char ch : t.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)+1);
         }
 
-        int count = mp.size();
-        int i = 0, j = 0;
-        int minLen = Integer.MAX_VALUE;
+        int count = map.size();
+        int ans = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+        int n = s.length();
         int start = 0;
 
-        while (j < s.length()) {
-            char endChar = s.charAt(j);
+        while(j < n){
+            if(map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j), map.get(s.charAt(j))-1);
 
-            if (mp.containsKey(endChar)) {
-                mp.put(endChar, mp.get(endChar) - 1);
-                if (mp.get(endChar) == 0) {
+                if(map.get(s.charAt(j)) == 0){
                     count--;
                 }
             }
 
-            // Try to shrink window
-            while (count == 0) {
-                char startChar = s.charAt(i);
+           
+            while(count == 0){
+                if(map.containsKey(s.charAt(i))){
+                    map.put(s.charAt(i), map.get(s.charAt(i))+1);
 
-                if (mp.containsKey(startChar)) {
-                    mp.put(startChar, mp.get(startChar) + 1);
-
-                    if (mp.get(startChar) == 1) {
-                        // Update minimum
-                        if (j - i + 1 < minLen) {
-                            minLen = j - i + 1;
-                            start = i;
+                    if(map.get(s.charAt(i)) == 1){
+                        if(j-i+1 < ans){
+                            ans = Math.min(ans, j-i+1);
+                            start = i; 
                         }
                         count++;
                     }
+
                 }
                 i++;
             }
-
             j++;
+            
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return ans == Integer.MAX_VALUE ? "" : s.substring(start, start + ans);
     }
 }
