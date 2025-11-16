d2b6651b621e2f78c4f3b03338074b1abb8f74d5
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-11-16 16:50:27 +0530

    Time: 9 ms (65.44%) | Memory: 45.9 MB (19.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -12,11 +12,24 @@ class Solution {
             hash[s.charAt(j) - 'A']++;
             maxF = Math.max(maxF, hash[s.charAt(j)-'A']);
 
-            // If not a valid substring, then just trim it(For optimization: I'm simply removing one
-            // char from last and adding one char from the front.)
-            // So just removing one char from the front.
-            if(j-i+1 - maxF > k){
+            // // If not a valid substring, then just trim it(For optimization: I'm simply removing one
+            // // char from last and adding one char from the front.)
+            // // So just removing one char from the front.
+            // if(j-i+1 - maxF > k){
+            //     hash[s.charAt(i) - 'A']--;
+            //     i++;
+            // }
+
+            // Above approach is most optimized.
+            // In this we will keep removing the characters from start till it is not valid substring.
+
+            while(j-i+1 - maxF > k){
                 hash[s.charAt(i) - 'A']--;
+                // here we have to update the maxF each time, so setting it as zero and recalculating.
+                maxF = 0;
+                for(int m = 0; m < 26; m++){
+                    maxF = Math.max(maxF, hash[m]);
+                }
                 i++;
             }
 
