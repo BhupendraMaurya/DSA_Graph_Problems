38ca58327e0f752f7c9bfa50ff6d3491718a0084
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-11-29 20:34:25 +0530

    Time: 72 ms (41.51%) | Memory: 78.4 MB (15.92%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,65 +1,38 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // find the next smaller left, and next smaller right, and then 
-        // for every index, (nxtsmaller right - next smaller left + 1)* arr[i];
+        // Optimal Solution: Using a single pass and not calculating the NSE and PSE.
+        // Simply going to each element and then calculating accordingly.
 
-        // finding the next smaller left;
-        int n = heights.length;
-
-        int leftSmall[] = new int[n];
-
+        int maxArea = 0;
+        // Taking on stack and storing only rhe indexes.
         Stack<Integer> st = new Stack<>();
-        for(int i=0;i<heights.length;i++){
 
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
+        for(int i = 0; i < heights.length; i++){
+            // If current height is smaller than the prev, that means we have to find its PSE
+            // so we have to pop the elements which are greater than current, and also if current element is 
+            // smaller than the prev that means it(curr) is the NSE of its prev. 
+            // So, finding the Area for the prev histogram.
+            // using the formula: heights[i] * (nse - pse - 1);
+            while(!st.isEmpty() && heights[st.peek()] > heights[i]){
+                int element = heights[st.peek()];
                 st.pop();
+                int nse = i;
+                int pse = st.isEmpty() ? -1 : st.peek();
+                maxArea = Math.max(maxArea, element * (nse - (pse) - 1));
             }
-
-            // is st is empty, that means, in heights array, no element is smaller than
-            //current element, so our left boundary would be first hiehgts(means 0 index)
-
-            if(st.isEmpty()){
-                leftSmall[i] = 0;
-            }
-            else{
-                leftSmall[i] = st.peek() + 1;
-            }
-
             st.push(i);
 
         }
 
-        st.clear();
-
-
-        // finding the right smaller element
-
-        int rightSmall[] = new int[n];
-
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
-
-                st.pop();
-            }
-
-            if(st.isEmpty()){
-                rightSmall[i] = n-1;
-            }
-
-            else{
-                rightSmall[i] = st.peek() -1;
-            }
-
-            st.push(i);
-
-        }
-
-
-        int maxArea = Integer.MIN_VALUE;
-
-        for(int i=0;i<n;i++){
-            int area = (rightSmall[i] - leftSmall[i] + 1) * heights[i];
-            maxArea = Math.max(area, maxArea);
+        // If there were some heights whose NSE or PSE was not available, then just take the NSE as n and pse as
+        // -1 and find the area of them as well.
+        int n = heights.length;
+        while(!st.isEmpty()){
+            int element = heights[st.peek()];
+            st.pop();
+            int nse = n;
+            int pse = st.isEmpty() ? -1 : st.peek();
+            maxArea = Math.max(maxArea, element * (nse - (pse) - 1));
         }
 
         return maxArea;
