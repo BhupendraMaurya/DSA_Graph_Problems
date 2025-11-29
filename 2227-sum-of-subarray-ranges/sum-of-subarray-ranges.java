3b1c1139de3733a26ea7cb1489e675f335d5a380
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-11-29 09:50:38 +0530

    Time: 15 ms (96.51%) | Memory: 46.1 MB (35.15%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -3,14 +3,12 @@ class Solution {
         
         long sum = 0;
         for(int i = 0;i  < nums.length; i++){
-            
+            int maxi = Integer.MIN_VALUE;
+            int mini = Integer.MAX_VALUE;
             for(int j = i; j < nums.length; j++){
-                int maxi = Integer.MIN_VALUE;
-                int mini = Integer.MAX_VALUE;
-                for(int k = i; k <= j; k++){
-                    maxi = Math.max(nums[k], maxi);
-                    mini = Math.min(nums[k], mini);
-                }
+                
+                maxi = Math.max(nums[j], maxi);
+                mini = Math.min(nums[j], mini);
 
                 sum += maxi - mini;
             }
