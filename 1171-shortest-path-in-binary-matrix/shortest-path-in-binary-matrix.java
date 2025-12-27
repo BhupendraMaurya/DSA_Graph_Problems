13fc030d411fe753a481738e9af197b5aa8c8f12
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2025-12-27 22:55:13 +0530

    Time: 15 ms (70.90%) | Memory: 47.5 MB (59.46%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,65 +1,64 @@
-class Solution {
-    static class Pair {
-        int x;
-        int y;
-        int length;
+class Pair{
+    int x;
+    int y;
+    int dist;
 
-        Pair(int x, int y, int length){
-            this.x = x;
-            this.y = y;
-            this.length = length;
-        }
-
-       
+    Pair(int x, int y, int dist){
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
     }
+}
+class Solution {
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-
         int n = grid.length;
+        int vis[][] = new int[n][n];
 
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
-            return -1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                vis[i][j] = Integer.MAX_VALUE;
+            }
         }
-        
-        // We were using the PW because we wanted the smallest length of different lengths, but here, every length of each cell would be increased buy 
-        // 1 only, so we do not require PQ. as it will increase the TC.
-        Queue<Pair> q = new LinkedList<>();
+        vis[0][0] = 0;
+
         int delR[] = {0, 1, 1, 1, 0, -1, -1, -1};
         int delC[] = {1, 1, 0, -1, -1, -1, 0, 1};
 
-        boolean vis[][] = new boolean[n][n];
+        Queue<Pair> q = new LinkedList<>();
+        // if starting cell is itself 1, then we cant move further, and simply return -1.
+        if(grid[0][0] == 1){
+            return -1;
+        }
 
-        int length = 1;
-
-        // adding the src in the pq and length as 1 in the starting.
-        q.add(new Pair(0,0, length));
-        vis[0][0] = true;
+        // if single element, then always possible.
+        if(n == 1) return 1;
+        q.add(new Pair(0, 0, 1));
 
         while(!q.isEmpty()){
             Pair curr = q.remove();
-            int u = curr.x;
-            int v = curr.y;
-            int currLen = curr.length;
+            int row = curr.x;
+            int col = curr.y;
+            int distance = curr.dist;
 
-            // if current index is the last indx(means last cell), then just return the length.
-            if(u == n-1 && v == n-1){
-                return currLen;
-            }
-
-            
-                // Traversing in all the 8 directions. of current cell.
             for(int i = 0; i < 8; i++){
-                int nr = u + delR[i];
-                int nc = v + delC[i];
+                int newR = row + delR[i];
+                int newC = col + delC[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0 && !vis[nr][nc]){
-                    q.add(new Pair(nr, nc, currLen+1));
-                    vis[nr][nc] = true;
+                if(newR >= 0 && newR < n && newC >= 0 && newC < n && grid[newR][newC] == 0
+                    && distance + 1 < vis[newR][newC])
+                {
+                    vis[newR][newC] = distance + 1;
+
+                    if(newR == n-1 && newC == n-1){
+                        return distance + 1;
+                    }
+
+                    q.add(new Pair(newR, newC, distance+1));
                 }
             }
-            
         }
-
         return -1;
-
+        
     }
 }
