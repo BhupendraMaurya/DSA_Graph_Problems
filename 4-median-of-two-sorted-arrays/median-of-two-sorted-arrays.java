5be5f21335f4e8bf2a7c04cd2011b563e93f306a
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-04 01:30:03 +0530

    Time: 1 ms (99.66%) | Memory: 49.2 MB (5.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -5,6 +5,17 @@ class Solution {
         int n = n1 + n2;
 
         // I will try to solve this problem without using the extra spaces;
+
+        /*
+        * Intuition: If somehow we are able to find the elements of indexes of n/2 and n/2-1
+        then my job will be done. 
+        That's why taking two variables to store the indexes and also storing these elements.
+
+        and taking one variable to store the overall current index so that we can track 
+        and find our desired index. In the last, simply returning the answers, stored in 
+        these variables(ele1,and ele2).
+
+         */
         int ind1 = n / 2 - 1;
         int ind2 = n/2;
         int cnt = 0;
