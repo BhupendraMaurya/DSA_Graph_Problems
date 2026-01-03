49474ae5fc712ee19429985a0d341cf7b3ca7218
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-03 16:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (49.70%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,53 +1,52 @@
 class Solution {
-    public boolean isValid(int arr[], int k, int maxPages){
-        int student = 1;
-        int sumOfPages = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            sumOfPages += arr[i];
-            
-            if(sumOfPages > maxPages){
-                student++;
-                sumOfPages = arr[i];
+    public boolean isValid(int nums[], int k, int maxSum){
+        int count = 1;
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(sum > maxSum){
+                count++;
+                sum = nums[i];
             }
-            
-            if(student > k){
+            if(count > k){
                 return false;
             }
         }
-        
+
         return true;
     }
-    
-    public int splitArray(int[] arr, int k) {
-        if(k > arr.length){
+
+    // This problem is exactly same like: Allocate minimum pages of GFG:
+    // https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+    public int splitArray(int[] nums, int k) {
+        if(k > nums.length){
             return -1;
         }
-        
         int maxi = 0;
         int sum = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            maxi = Math.max(maxi, arr[i]);
-            sum += arr[i];
+
+        for(int i = 0; i < nums.length; i++){
+            maxi = Math.max(nums[i], maxi);
+            sum += nums[i];
         }
-        
+
+        int ans = Integer.MIN_VALUE;
         int low = maxi;
         int high = sum;
-        int ans = -1;
-        
+
         while(low <= high){
             int mid = high + (low - high) / 2;
             
-            if(isValid(arr, k, mid)){
+            if(isValid(nums, k, mid)){
                 ans = mid;
                 high = mid - 1;
             }
             else{
                 low = mid + 1;
             }
-        }
-        
+        };
+
         return ans;
     }
 }
