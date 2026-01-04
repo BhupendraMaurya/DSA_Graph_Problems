318e4445ebc92e4534396abbd441338063590456
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-04 20:27:31 +0530

    Time: 1 ms (99.66%) | Memory: 48.7 MB (73.45%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,64 +1,51 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
+        // Since I want to apply Binary Search on the smaller array, that's why, I'm just
+        // checking for smaller length array and then calling the same function to reverse the arrays.
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        // I will try to solve this problem without using the extra spaces;
+        int m = nums1.length;
+        int n = nums2.length;
 
-        /*
-        * Intuition: If somehow we are able to find the elements of indexes of n/2 and n/2-1
-        then my job will be done. 
-        That's why taking two variables to store the indexes and also storing these elements.
+        // Applying the BS in the smaller length array.
+        int l = 0;// either taking zero elements from the first array
+        int r = m;// or taking all the elements from the first array
+        
+        while(l <= r){
+            // count of elements taken from the first array
+            int px = l + (r-l)/2;
 
-        and taking one variable to store the overall current index so that we can track 
-        and find our desired index. In the last, simply returning the answers, stored in 
-        these variables(ele1,and ele2).
+            // count of elements taken from second array
+            int py = (m+n+1)/2 - px;
 
-         */
-        int ind1 = n / 2 - 1;
-        int ind2 = n/2;
-        int cnt = 0;
-        int ele1 = -1;
-        int ele2 = -1;
+            // elements from the left part from both the array
+            int x1 = (px == 0) ? Integer.MIN_VALUE : nums1[px-1];
+            int x2 = (py == 0) ? Integer.MIN_VALUE : nums2[py-1];
 
-        int i = 0;
-        int j = 0;
-        while(i < n1 && j < n2){
-            if(nums1[i] < nums2[j]){
-                if(cnt == ind1) ele1 = nums1[i];
-                if(cnt == ind2) ele2 = nums1[i];
-                i++;
-                cnt++;
+            // elements from the right part after separation
+            int x3 = (px == m) ? Integer.MAX_VALUE : nums1[px];
+            int x4 = (py == n) ? Integer.MAX_VALUE : nums2[py];
+
+            if(x1 <= x4 && x2 <= x3){
+                if((m+n) % 2 == 1){
+                    return Math.max(x1, x2);
+                }
+
+                return (double)(Math.max(x1,x2) + Math.min(x3, x4))/2;
+            }
+
+            if(x1 > x4){
+                r = px - 1;
             }
             else{
-                if(cnt == ind1) ele1 = nums2[j];
-                if(cnt == ind2) ele2 = nums2[j];
-                j++;
-                cnt++;
+                l = px + 1;
             }
+
         }
 
-        while(i < n1){
-            if(cnt == ind1) ele1 = nums1[i];
-            if(cnt == ind2) ele2 = nums1[i];
-            i++;
-            cnt++;
-        }
-
-        while(j < n2){
-            if(cnt == ind1) ele1 = nums2[j];
-            if(cnt == ind2) ele2 = nums2[j];
-            j++;
-            cnt++;
-        }
-
-        if(n % 2 == 1){
-            return (double) ele2;
-        }
-
-        else{
-            return (double) (ele1 + ele2) / 2;
-        }
+        return (double) -1;
+        
     }
 }
