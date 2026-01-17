78b2709e00ed815918eeec7b8318f0bf21c95799
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-17 10:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (38.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int memo(int n, int dp[]){
-        if(n == 0 || n == 1){
-            return n;
-        }
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = memo(n-1, dp) + memo(n-2, dp);
-        return dp[n];
-    }
+    
     public int fib(int n) {
         if(n == 0){
             return 0;
         }
-        // memoization approach
+        //tabulation approach
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
+        
         dp[0] = 0;
         dp[1] = 1;
 
-        memo(n, dp);
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
         return dp[n];
     }
 }
