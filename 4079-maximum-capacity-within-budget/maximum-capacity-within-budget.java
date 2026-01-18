5b477ba99f6863a0c4fea4bdfa7a80f5aa12e4b8
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-18 19:14:30 +0530

    Time: 128 ms (33.29%) | Memory: 180.1 MB (52.67%) - LeetSync

---
+++ 4079-maximum-capacity-within-budget/maximum-capacity-within-budget.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int upperBound(int a[][], int val, int hi){
+        int low = 0;
+        int high = hi;
+
+        int ans = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(a[mid][0] <= val){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+    public int maxCapacity(int[] costs, int[] capacity, int budget) {
+        int n = costs.length;
+
+        int a[][] = new int[n][2];
+        for(int i =  0; i < n; i++){
+            a[i][0] = costs[i];
+            a[i][1] = capacity[i];
+        }
+
+        Arrays.sort(a, (x, y) -> x[0] - y[0]);
+
+        int best[] = new int[n];
+        best[0] = a[0][1];
+
+        for(int i = 1; i < n; i++){
+            best[i] = Math.max(best[i-1], a[i][1]);
+        }
+
+        int ans = 0;
+        // case 1: picking one one item.
+        for(int i = 0; i < n; i++){
+            if(a[i][0] < budget){
+                ans = Math.max(ans, a[i][1]);
+            }
+        }
+
+        // case 2: picking two items.
+        for(int j = 1; j < n; j++){
+            int rem = budget - a[j][0] - 1;
+            int i = upperBound(a, rem, j-1);
+
+            if(i >= 0){
+                ans = Math.max(ans, best[i] + a[j][1]);
+            }
+        }
+
+        return ans;
+    }
+}
