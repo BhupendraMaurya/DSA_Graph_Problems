a9fe0d6cc51336c26d45ad5b4dac33c87be258c4
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-26 10:57:39 +0530

    Time: 4 ms (75.36%) | Memory: 46.5 MB (93.24%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,56 +1,38 @@
 class Solution {
-    public int distance(int i, int j, String s1, String s2, int dp[][]){
-        if(i < 0){
-            return j+1;
-        }
-        if(j < 0){
-            return i+1;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = distance(i-1, j-1, s1, s2, dp);
-        }
-        else{
-            int insert = 1 + distance(i, j-1, s1, s2, dp);
-            int replace = 1 + distance(i-1, j-1, s1, s2, dp);
-            int delete = 1 + distance(i-1, j, s1, s2, dp);
-
-            return dp[i][j] = Math.min(insert, Math.min(replace, delete));
-        }
-    }
+    
     public int minDistance(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int dp[][] = new int[n+1][m+1];
+        int prev[] = new int[m+1];
         
         for(int j = 0; j <= m; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
+        // for(int i = 0; i <= n; i++){
+        //     dp[i][0] = i;
+        // }
 
         for(int i = 1; i <= n; i++){
+            int curr[] = new int[m+1];
+            curr[0] = i;
             for(int j = 1; j <= m ; j++){
+                
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 } 
                 else{
-                    int insert = 1 + dp[i][j-1];
-                    int replace = 1 + dp[i-1][j-1];
-                    int delete = 1 + dp[i-1][j];
+                    int insert = 1 + curr[j-1];
+                    int replace = 1 + prev[j-1];
+                    int delete = 1 + prev[j];
 
-                    dp[i][j] = Math.min(insert, Math.min(replace, delete));
+                    curr[j] = Math.min(insert, Math.min(replace, delete));
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
         
     }
 }
