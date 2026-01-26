6529fad5eb0dfd7ab39aa6c40bcd58a9261d1e9e
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-01-26 17:07:36 +0530

    Time: 79 ms (28.89%) | Memory: 108.8 MB (41.29%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,53 +1,33 @@
 class Solution {
-    public int price(int i, int buy, int cap, int prices[], int dp[][][]){
-        if(cap == 0){
+    public int price(int i, int buyOrsell, int prices[], int dp[][]){
+        
+        if(i == prices.length || buyOrsell >= 5){
             return 0;
         }
-
-        if(i == prices.length){
-            return 0;
-        }
-        if(dp[i][buy][cap] != -1){
-            return dp[i][buy][cap];
+        if(dp[i][buyOrsell] != -1){
+            return dp[i][buyOrsell];
         }
 
-        if(buy == 1){
-            int b1 = -prices[i] + price(i+1, buy-1, cap, prices, dp);
-            int b2 = price(i+1, buy, cap, prices, dp);
-            return dp[i][buy][cap] = Math.max(b1, b2);
+        if(buyOrsell % 2 == 0){
+            int b1 = -prices[i] + price(i+1, buyOrsell+1, prices, dp);
+            int b2 = price(i+1, buyOrsell, prices, dp);
+            return dp[i][buyOrsell] = Math.max(b1, b2);
         }
         else{
-            int s1 = prices[i] + price(i+1, buy+1, cap-1, prices, dp);
-            int s2 = price(i+1, buy, cap, prices, dp);
+            int s1 = prices[i] + price(i+1, buyOrsell+1, prices, dp);
+            int s2 = price(i+1, buyOrsell, prices, dp);
 
-            return dp[i][buy][cap] = Math.max(s1, s2);
+            return dp[i][buyOrsell] = Math.max(s1, s2);
         }
     }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int next[][] = new int[3][3];
         
 
-
-        for(int i = n-1; i >= 0; i--){
-            int curr[][] = new int[3][3];
-            for(int buy = 0; buy <= 1; buy++){
-                for(int cap = 2; cap >= 1; cap--){
-                    if(buy == 1){
-                        int b1 = -prices[i] + next[buy-1][cap];
-                        int b2 = next[buy][cap];
-                        curr[buy][cap] = Math.max(b1, b2);
-                    }
-                    else{
-                        int s1 = prices[i] + next[buy+1][cap-1];
-                        int s2 = next[buy][cap];
-
-                        curr[buy][cap] = Math.max(s1, s2);
-                    }
-                }
-            }
-            next = curr;
+        int dp[][] = new int[n][5];
+        for(int rows[]: dp){
+            Arrays.fill(rows, -1);
         }
-        return next[1][2];
+        return price(0, 0, prices, dp);
     }
 }
