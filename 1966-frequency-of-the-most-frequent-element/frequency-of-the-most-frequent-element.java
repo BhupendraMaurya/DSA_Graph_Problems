4d674f202052ca140d899e6493911e50c2c47a53
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-11 14:35:08 +0530

    Time: 38 ms (7.63%) | Memory: 94.8 MB (51.52%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,31 +1,21 @@
 class Solution {
     // using sliding window approach
     public int maxFrequency(int[] nums, int k) {
-        // sorting the array so that all the nearest elements could come closer.
-        Arrays.sort(nums);
-
-        
-        int left = 0;
-        int right = 0;
-        int n = nums.length;
-
-        long ans = 0;
-        long sum = 0;
-
-
-        while(right < n){
-            sum += nums[right];
-
-
-            while((right - left +1L) * nums[right] - sum > k){
-                sum -= nums[left];
+       Arrays.sort(nums);
+       int left = 0;
+       int right = 0;
+       long totalSum = 0;
+       long freq = 0;
+       while(right < nums.length){
+            totalSum += nums[right];
+            while(left <= right && nums[right] * (right-left+1L) > totalSum + k){
+                totalSum -= nums[left];
                 left++;
             }
-
-            ans = Math.max(ans, right - left + 1L);
+            freq = Math.max(freq, right-left+1L);
             right++;
-        }
+       }
+       return (int)freq;
 
-        return (int)ans;
     }
 }
