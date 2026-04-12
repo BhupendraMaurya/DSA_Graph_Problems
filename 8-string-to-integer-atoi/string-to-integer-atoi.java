e6a7bb7995db9b8141d87493842e2c3ac4926e71
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-12 12:29:53 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (80.36%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,34 +1,51 @@
 class Solution {
     public int myAtoi(String s){
+        //first check for spaces
         int i = 0;
-        int sign = 1;
         int n = s.length();
+        if(n == 0){
+            return 0;
+        }
         while(i < n && s.charAt(i) == ' '){
             i++;
         }
-
-        if(i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
-            if(s.charAt(i) == '-'){
-                sign = -1;
-            }
-            else{
-                sign = 1;
-            }
+        if(i == n){
+            return 0;
+        }
+        int sign = 1;
+        // check for signs
+        if(s.charAt(i) == '-'){
+            sign = -1;
+            i++;
+        }
+        else if(s.charAt(i) == '+'){
+            sign = 1;
             i++;
         }
 
-        long num = 0;
+        // check for leading zeroes;
+        while(i < n && s.charAt(i) == '0'){
+            i++;
+        }
+
+        StringBuilder sb = new StringBuilder();
         while(i < n && Character.isDigit(s.charAt(i))){
-            num = num * 10 + (s.charAt(i) - '0');
-            if(num * sign > Integer.MAX_VALUE){
+            sb.append(s.charAt(i));
+            i++;
+        }
+
+        long ans = 0;
+        for(int j = 0; j < sb.length(); j++){
+            ans = ans * 10 + (sb.charAt(j) - '0');
+            if(ans * sign > Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
             }
-            else if(num*sign < Integer.MIN_VALUE){
+            if(ans * sign < Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
             }
             i++;
         }
-        return (int)num*sign;
+        return (int)ans * sign;
 
     } 
 }
