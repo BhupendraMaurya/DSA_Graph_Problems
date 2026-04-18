82ca410d51311136731a4bf7bdbcfbefad4567b2
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-18 16:17:46 +0530

    Time: 3 ms (70.55%) | Memory: 47.8 MB (41.97%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,45 +1,25 @@
 class Solution {
 
-    // Generate all the combinations of size n, and then check binary strings has adjacent zeroes and which not.
-    public void generateCombination(int i, String str, HashSet<String> set, int n){
+    // Generating only the correct string with the help of previous taken character(0 or 1).
+    public void generateCombination(int i, String str, List<String> list, int n){
         if(i == n){
-            set.add(str);
+            list.add(str);
             return;
         }
-        generateCombination(i+1, str + '0', set, n);
 
-        generateCombination(i+1, str + '1', set, n);
-
-    }
-    public boolean hasAdjacentZeroes(String str){
-        // if string has length 2
-        if(str.length() == 2 && str.charAt(0) == '0' && str.charAt(0) == str.charAt(1)){
-            return true;
+        int size = str.length();
+        if(i != 0 && str.charAt(size-1) == '0'){//if last taken char is '0', then we can only take '1', as we cant take adjacent zeroes.
+            generateCombination(i+1, str + '1', list, n);
         }
-        for(int i = 1; i < str.length()-1; i++){
-            char prev = str.charAt(i-1);
-            char curr = str.charAt(i);
-            char next = str.charAt(i+1);
-
-            if(curr == '0' && (prev == curr || curr == next)){
-                return true;
-            }
+        else{// if last char is '1', then we can take any, '0' or '1'.
+            generateCombination(i+1, str + '0', list, n);
+            generateCombination(i+1, str + '1', list, n);
         }
-        return false;
+
     }
     public List<String> validStrings(int n) {
-        HashSet<String> set = new HashSet<>();
-        generateCombination(0, "", set, n);
-
         List<String> list = new ArrayList<>();
-        for(String str : set){
-            if(hasAdjacentZeroes(str)){
-                continue;
-            }
-            else{
-                list.add(str);
-            }
-        }
+        generateCombination(0, "", list, n);
         return list;
     }
 }
