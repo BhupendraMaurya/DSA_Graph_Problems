e6ffb4725b675e2d856732036980ab3e415bd5f6
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-18 17:25:51 +0530

    Time: 173 ms (6.09%) | Memory: 51.1 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,15 +1,15 @@
 class Solution {
     public void generate(int i, String str, int n, List<String> list){
-        // since we already had taken one char['('] in starting, that'w why going till n-1. 
-        if(i == n-1){
+        if(i == n){
             System.out.println(str);
             list.add(str);
             return;
         }
-        // take the closing braces: ')'
-        generate(i+1, str + ')', n, list);
+        
         // taking the opening braces: '('
         generate(i+1, str + '(', n, list);
+        // take the closing braces: ')'
+        generate(i+1, str + ')', n, list);
     }
     public boolean isValid(String str){
         Stack<Character> st = new Stack<>();
@@ -36,7 +36,7 @@ class Solution {
     }
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<>();
-        generate(0, "(", 2 * n, list);
+        generate(0, "", 2 * n, list);
 
         List<String> ans = new ArrayList<>();
         for(int i = 0; i < list.size(); i++){
