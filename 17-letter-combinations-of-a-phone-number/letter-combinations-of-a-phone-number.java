5abc3130b9a9f043c602dd47906f8cae3c3fbf3c
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-23 22:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (54.50%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void generate(String map[], List<String> list, String digits, StringBuilder curr, int n){
+        if(n == digits.length()){
+            list.add(curr.toString());
+            return;
+        }
+        int d = digits.charAt(n) - '0';
+        for(int i = 0; i < map[d].length(); i++){
+            curr.append(map[d].charAt(i));
+            generate(map, list, digits, curr, n+1);
+            curr.setLength(curr.length()-1);
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        String map[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        generate(map, result, digits, new StringBuilder(), 0);
+        return result;
+    }
+}
