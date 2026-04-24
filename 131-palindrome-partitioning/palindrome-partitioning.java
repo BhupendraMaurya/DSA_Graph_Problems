a03fda07078fe00e860aef0cf6cd6db7134b937d
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-24 23:57:53 +0530

    Time: 12 ms (9.21%) | Memory: 65.3 MB (58.62%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s){
+        int i = 0;
+        int j = s.length()-1;
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public void generate(int i, String s, List<List<String>> result, List<String> list){
+        if(i >= s.length()){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int j = i; j < s.length(); j++){
+            sb.append(s.charAt(j));
+            if(isPalindrome(sb.toString())){
+                list.add(sb.toString());
+                generate(j+1, s, result, list);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        generate(0, s, result, list);
+        return result;
+    }
+}
