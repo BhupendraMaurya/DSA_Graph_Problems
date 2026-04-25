4aede1d211b7b85bb91ead6ddd8a81b43a75f615
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-04-25 12:00:28 +0530

    Time: 144 ms (45.37%) | Memory: 42.9 MB (76.82%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,35 @@
+
+class Solution {
+    public boolean search(int i, int j, int idx, char board[][], String word, boolean vis[][]){
+        // if idx is reached out of boundary, that means, we have got the required string, so returning true.
+        if(idx >= word.length()){
+            return true;
+        }
+        // if out of bounds, or already visited, or not matched, then just returning false. 
+        if(i >= board.length || j >= board[0].length || i < 0 || j < 0 || vis[i][j] || board[i][j] != word.charAt(idx)){
+            return false;
+        }
+        // marking the current cell as visited and recursively calling the function for all of its 4 neighbours
+        vis[i][j] = true;
+        boolean result = search(i-1, j, idx+1, board, word, vis) || search(i, j+1, idx+1, board, word, vis) ||
+                        search(i+1, j, idx+1, board, word, vis) || search(i, j-1, idx+1, board, word, vis);
+        // backtracking
+        vis[i][j] = false;
+        // returning the result. 
+        return result;
+    }
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i = 0; i < n ; i++){
+            boolean vis[][] = new boolean[n][m];
+            for(int j = 0; j < m; j++){
+                if(search(i, j, 0, board, word, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
