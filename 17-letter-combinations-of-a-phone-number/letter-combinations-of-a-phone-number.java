f647894cc03fd8ad94c8a822930b854ba0be3ad5
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-05-09 09:50:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (65.77%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,31 +1,32 @@
 class Solution {
-    public void generate(List<String> list, String map[], String digits, StringBuilder sb, int n){
-        // here n is the size of the string generated till now, so if n is equal to length of original string, that means, 
-        // we have got one combination, so storing it in the list. 
-        if(n == digits.length()){
-            list.add(sb.toString());
+    public void generate(int idx, String digits, StringBuilder sb, List<String> result, String map[]){
+        if(idx == digits.length()){
+            result.add(sb.toString());
             return;
         }
-        // here digit represents that on which index you are at currently
-        int digit = digits.charAt(n) - '0';
-        // with the help of above index go at this index and access that string and run loop on this,
-        for(int i = 0; i < map[digit].length(); i++){
-            // taking the current char and append it
-            sb.append(map[digit].charAt(i));
-            // recursively call the function with sizee +1 
-            generate(list, map, digits, sb, n+1);
-            // backtracking steps
+
+        int currNum = digits.charAt(idx) - '0';
+        String currStr = map[currNum];
+        for(int i = 0; i < currStr.length(); i++){
+            sb.append(currStr.charAt(i));
+            generate(idx+1, digits, sb, result, map);
             sb.deleteCharAt(sb.length()-1);
-        } 
+        }
     }
     public List<String> letterCombinations(String digits) {
-        // Using the Recursion + backtracking, 
-        // Creating an array, which represents each number and then just doing the recursion. 
-        // we will start from the empty string, as we dont have any string, and then we will keep taking the numbers and 
-        // will try to connect this with its next combination(if exist). 
-        List<String> list = new ArrayList<>();
-        String map[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        generate(list, map, digits, new StringBuilder(), 0);
-        return list;
+        String map[] = new String[10];
+        map[2] = "abc";
+        map[3] = "def";
+        map[4] = "ghi";
+        map[5] = "jkl";
+        map[6] = "mno";
+        map[7] = "pqrs";
+        map[8] = "tuv";
+        map[9] = "wxyz";
+        StringBuilder sb = new StringBuilder();
+        List<String> result = new ArrayList<>();
+        generate(0, digits, sb, result, map);
+
+        return result;
     }
 }
