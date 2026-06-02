9e0f5dbdb08168399efdea2146e7a2a892be93e0
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-06-02 22:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (68.89%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -20,31 +20,31 @@ class Solution {
         if(root == null){
             return post;
         }
-        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> st = new Stack<>();
 
-        TreeNode node = root;
-        // took two stack and then just push root into the stack 1 and then start taking the top element out,
-        // and also push it into the st2 and then check its left and right element, if not null, then first insert
-        // left and then right, 
-        // in the last, just keep  take all the elements from the stack and print it. 
-
-        // We can also solve this without using the second stack. as it is only used to store the nodes. 
-        // so store nodes in list and then reverse and return it. 
-
-        s1.push(node);
-        while(!s1.isEmpty()){
-            TreeNode top = s1.pop();
-            post.add(top.val);
-            if(top.left != null){
-                s1.push(top.left);
+        TreeNode curr = root;
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
             }
+            else{
+                TreeNode temp = st.peek().right;
 
-            if(top.right != null){
-                s1.push(top.right);
+                if(temp == null){
+                    temp = st.pop();
+                    post.add(temp.val);
+
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.pop();
+                        post.add(temp.val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
             }
         }
-
-        Collections.reverse(post);
         return post;
     }
 }
