cb49fb40af46ad13d92f215f79d95e918f19784e
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-06-04 22:18:07 +0530

    Time: 3 ms (98.75%) | Memory: 87.5 MB (38.66%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
@@ -1,18 +1,24 @@
 class Solution {
     public long minOperations(int[] nums) {
-        long ans = 0; // answer to store the total operations requiered to make the array non decreasing. 
-        long curr = 0; // total operations we have now(left from previous operations);
+        long ans = 0;   // total operations needed
+        
+        int k = 0;
+        int j = nums.length-1;
+        while(k < j){
+            int temp = nums[k];
+            nums[k] = nums[j];
+            nums[j] = temp;
+            k++;
+            j--;
+        }
 
         for(int i = 1; i < nums.length; i++){
-            // calculating that how much extra needed to make the current element equal or greater than its previous higher.
-            long need = Math.max(0, curr + nums[i-1] - nums[i]);
-            if(need > curr){//if need is greater thanb current I have, then just add the difference between them. 
-                ans += need - curr;
+            if(nums[i] > nums[i-1]){
+                ans += nums[i] - nums[i-1];
             }
-            // and whatever operations I have left, I can apply it in next, if possible
-            curr = need;
         }
 
         return ans;
+
     }
 }
