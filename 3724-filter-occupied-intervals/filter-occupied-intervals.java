1ccdd0173578587d48cd4cb88d19bdf29c55fbc2
Bhupendra <105388846+BhupendraMaurya@users.noreply.github.com>
2026-06-28 11:23:12 +0530

    Time: 105 ms (5.13%) | Memory: 180.8 MB (5.05%) - LeetSync

---
+++ 3724-filter-occupied-intervals/filter-occupied-intervals.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<Integer>> filterOccupiedIntervals(int[][] intervals, int freeStart, int freeEnd) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(intervals.length == 0){
+            return list;
+        }
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        
+        int st = intervals[0][0];
+        int prevTime = intervals[0][1];
+        int n = intervals.length;
+        for(int i = 1; i < intervals.length; i++){
+            int stime = intervals[i][0];
+            int endTime = intervals[i][1];
+            if(stime <= prevTime+1){
+                prevTime = Math.max(prevTime, endTime);
+            }
+            else{
+                list.add(new ArrayList<>(Arrays.asList(st, prevTime)));
+                st = stime;
+                prevTime = endTime;
+            }
+        }
+        list.add(new ArrayList<>(Arrays.asList(st, prevTime)));
+
+        List<List<Integer>> result = new ArrayList<>();
+        for(List<Integer> curr : list){
+            int stime = curr.get(0);
+            int et = curr.get(1);
+            if(freeEnd < stime || freeStart > et){
+                result.add(curr);
+            }
+            else if(stime >= freeStart && et <= freeEnd){// complete overlapped
+                continue;
+            }
+            else{// partially. 
+                if (stime < freeStart && et <= freeEnd){
+                    result.add(new ArrayList<>(Arrays.asList(stime, freeStart-1)));
+                }
+                else if((stime >= freeStart && et > freeEnd)){
+                    result.add(new ArrayList<>(Arrays.asList(freeEnd+1, et)));
+                } 
+                else{// completely inside given interval: if (stime < freeStart && et > freeEnd)
+                    
+                    result.add(new ArrayList<>(Arrays.asList(stime, freeStart-1)));
+                    result.add(new ArrayList<>(Arrays.asList(freeEnd+1, et)));
+                }
+            }
+        }
+        System.out.println(list);
+        return result;
+    }
+}
